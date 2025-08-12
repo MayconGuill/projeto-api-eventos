df816c8112bc717ad8de98789a7aefb30157748b
MayconGuill <mayconguill@gmail.com>
2025-08-12 16:26:20 -0300

    Domain created

---
+++ src/main/java/com/eventostac/api/domain/address/Address.java
@@ -0,0 +1,30 @@
+package com.eventostac.api.domain.address;
+
+import com.eventostac.api.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Table(name = "address")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String city;
+    private String uf;
+
+    @OneToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
