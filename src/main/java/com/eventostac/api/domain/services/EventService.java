70103885a6dc43729ef5afbb9cc949d62d0bb1e7
MayconGuill <mayconguill@gmail.com>
2025-08-14 13:28:52 -0300

    Post event create

--- src/main/java/com/eventostac/api/domain/services/EventService.java
+++ src/main/java/com/eventostac/api/domain/services/EventService.java
@@ -3,6 +3,7 @@ package com.eventostac.api.domain.services;
 import com.amazonaws.services.s3.AmazonS3;
 import com.eventostac.api.domain.event.Event;
 import com.eventostac.api.domain.event.EventRequestDTO;
+import com.eventostac.api.repositories.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -18,11 +19,14 @@ import java.util.UUID;
 @Service
 public class EventService {
 
-    @Value("${aws.bucket.name")
+    @Value("${aws.bucket.name}")
     private String bucketName;
 
     @Autowired
     private AmazonS3 s3Client;
+
+    @Autowired
+    private EventRepository repository;
 
     public Event createEvent(EventRequestDTO data) {
         String imgUrl = null;
@@ -37,6 +41,9 @@ public class EventService {
         newEvent.setEventUrl(data.eventUrl());
         newEvent.setDate(new Date(data.date()));
         newEvent.setImgUrl(imgUrl);
+        newEvent.setRemote(data.remote());
+
+        repository.save(newEvent);
 
         return newEvent;
     }
@@ -50,8 +57,9 @@ public class EventService {
             file.delete();
             return s3Client.getUrl(bucketName, fileName).toString();
         } catch(Exception e) {
+            e.printStackTrace();
             System.out.println("Erro ao subir arquivo");
-            return null;
+            return "";
         }
     }
 
--- src/main/java/com/eventostac/api/repositories/EventRepository.java
+++ src/main/java/com/eventostac/api/repositories/EventRepository.java
@@ -2,7 +2,6 @@ package com.eventostac.api.repositories;
 
 import com.eventostac.api.domain.event.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
