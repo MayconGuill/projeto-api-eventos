b8c48a8df1ab3a2e705eb2e060d02409f209bdfe
MayconGuill <mayconguill@gmail.com>
2025-08-20 08:49:50 -0300

    H2 Database migration to PostgreSQL

--- src/main/resources/db/migration/V1__create-event-table.sql
+++ src/main/resources/db/migration/V1__create-event-table.sql
@@ -1,5 +1,7 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
 CREATE TABLE event(
-    id UUID DEFAULT RANDOM_UUID() PRIMARY KEY,
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
     description VARCHAR(250) NOT NULL,
     img_url VARCHAR(100) NOT NULL,
--- src/main/resources/db/migration/V3__create-address-table.sql
+++ src/main/resources/db/migration/V3__create-address-table.sql
@@ -1,5 +1,7 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
 CREATE TABLE address(
-    id UUID DEFAULT RANDOM_UUID() PRIMARY KEY,
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     city VARCHAR(100) NOT NULL,
     uf VARCHAR(100) NOT NULL,
     event_id UUID,
